68683e0ce0520e3610c5595d5f90206f032b8cac
OASIS (Retr0night) <46064786+Oasis1819@users.noreply.github.com>
2020-03-27 12:07:23 +0900

    프로젝트 파일 업로드
    프로젝트 시작을 위한 기초 셋팅된 파일 업로드

---
+++ Fermata/dest/app.js
@@ -0,0 +1,8 @@
+"use strict";
+/*
+    Fermata Server
+    코로나19 실시간 접촉자 확인알림서비스 서버
+*/
+Object.defineProperty(exports, "__esModule", { value: true });
+//Initialize Settings
+//Start Server
---
+++ Fermata/src/app.ts
@@ -0,0 +1,13 @@
+/*
+    Fermata Server
+    코로나19 실시간 접촉자 확인알림서비스 서버
+*/
+
+//Import Modules
+import express from "express"
+import dotEnv from "dotenv"
+import mysql from "mysql"
+
+//Initialize Settings
+
+//Start Server
